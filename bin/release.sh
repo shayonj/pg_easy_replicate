6d70f04319e77730e75c66f73df8910eb57830cc
Shayon Mukherjee <shayonj@gmail.com>
2023-06-22 18:54:04 -0400

    Docker multi-platform image build and push from CI

--- bin/release.sh
+++ bin/release.sh
@@ -1,10 +1,6 @@
 export VERSION=$1
 echo "VERSION: ${VERSION}"
 
-echo "=== Pushing tags to github ===="
-git tag v"$VERSION"
-git push origin --tags
-
 echo "=== Building Gem ===="
 gem build pg_easy_replicate.gemspec
 
@@ -14,15 +10,9 @@ gem push pg_easy_replicate-"$VERSION".gem
 echo "=== Sleeping for 5s ===="
 sleep 5
 
-echo "=== Building Image ===="
-docker build . --build-arg VERSION="$VERSION" -t shayonj/pg_easy_replicate:"$VERSION"
-
-echo "=== Tagging Image ===="
-docker image tag shayonj/pg_easy_replicate:"$VERSION" shayonj/pg_easy_replicate:latest
-
-echo "=== Pushing Image ===="
-docker push shayonj/pg_easy_replicate:"$VERSION"
-docker push shayonj/pg_easy_replicate:latest
+echo "=== Pushing tags to github ===="
+git tag v"$VERSION"
+git push origin --tags
 
 echo "=== Cleaning up ===="
 rm pg_easy_replicate-"$VERSION".gem
