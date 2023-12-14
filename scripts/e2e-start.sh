32a9618e86c9d49f9993a8ad091c06de9efd217e
Shayon Mukherjee <shayonj@users.noreply.github.com>
2023-12-13 20:06:28 -0500

    Quote indent DB name (#76)

--- scripts/e2e-start.sh
+++ scripts/e2e-start.sh
@@ -3,11 +3,11 @@
 set -eo pipefail
 
 if [[ -z ${GITHUB_WORKFLOW} ]]; then
-  export SECONDARY_SOURCE_DB_URL="postgres://james-bond:james-bond123%407%21%273aaR@source_db/postgres"
+  export SECONDARY_SOURCE_DB_URL="postgres://james-bond:james-bond123%407%21%273aaR@source_db/postgres-db"
 fi
 
-export SOURCE_DB_URL="postgres://james-bond:james-bond123%407%21%273aaR@localhost:5432/postgres"
-export TARGET_DB_URL="postgres://james-bond:james-bond123%407%21%273aaR@localhost:5433/postgres"
+export SOURCE_DB_URL="postgres://james-bond:james-bond123%407%21%273aaR@localhost:5432/postgres-db"
+export TARGET_DB_URL="postgres://james-bond:james-bond123%407%21%273aaR@localhost:5433/postgres-db"
 export PGPASSWORD='james-bond123@7!'"'"''"'"'3aaR'
 
 # Bootstrap and cleanup
