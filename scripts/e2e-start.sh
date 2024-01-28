a6694c06e4eb3342157e1ab1bfebf25c3cb76457
Shayon Mukherjee <shayonj@gmail.com>
2024-01-28 10:09:54 -0500

    Perform config check in CI

--- scripts/e2e-start.sh
+++ scripts/e2e-start.sh
@@ -10,8 +10,9 @@ export SOURCE_DB_URL="postgres://james-bond:james-bond123%407%21%273aaR@localhos
 export TARGET_DB_URL="postgres://james-bond:james-bond123%407%21%273aaR@localhost:5433/postgres-db"
 export PGPASSWORD='james-bond123@7!'"'"''"'"'3aaR'
 
-# Bootstrap and cleanup
+# Config check, Bootstrap and cleanup
 echo "===== Performing Bootstrap and cleanup"
+bundle exec bin/pg_easy_replicate bootstrap config-check
 bundle exec bin/pg_easy_replicate bootstrap -g cluster-1 --copy-schema
 bundle exec bin/pg_easy_replicate start_sync -g cluster-1 -s public --recreate-indices-post-copy
 bundle exec bin/pg_easy_replicate stats -g cluster-1
