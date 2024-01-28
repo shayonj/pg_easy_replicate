f993a3771ec2435b0b8591fb555c7d5c4f9ec2a2
Shayon Mukherjee <shayonj@gmail.com>
2024-01-28 10:31:35 -0500

    Remove config check from start

--- scripts/e2e-start.sh
+++ scripts/e2e-start.sh
@@ -12,7 +12,6 @@ export PGPASSWORD='james-bond123@7!'"'"''"'"'3aaR'
 
 # Config check, Bootstrap and cleanup
 echo "===== Performing Bootstrap and cleanup"
-bundle exec bin/pg_easy_replicate bootstrap config-check
 bundle exec bin/pg_easy_replicate bootstrap -g cluster-1 --copy-schema
 bundle exec bin/pg_easy_replicate start_sync -g cluster-1 -s public --recreate-indices-post-copy
 bundle exec bin/pg_easy_replicate stats -g cluster-1
